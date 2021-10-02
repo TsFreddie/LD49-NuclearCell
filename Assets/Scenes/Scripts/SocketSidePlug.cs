7166acaec289b0e58edd0d05c57ad36c146056e0
Freddie Kins <tsfreddiewang@gmail.com>
2021-10-02 23:31:44 +0800

    Drag plug

--- Assets/Scenes/Scripts/SocketSidePlug.cs
+++ Assets/Scenes/Scripts/SocketSidePlug.cs
@@ -7,18 +7,49 @@ namespace NuclearCell
         public Transform PlugTransform;
         public Vector3 GrabAnchor;
         public bool Started = false;
+        public bool Picked = false;
 
         private Rigidbody _rigidbody;
         private Collider _collider;
+        private Vector3 _targetPos;
 
         protected void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
             _collider = GetComponent<Collider>();
+            _targetPos = transform.position;
+        }
+
+        protected void Update()
+        {
+            if (Started)
+            {
+                if (Picked)
+                {
+                    var mousePos = GameManager.Singleton.WallCam.ScreenToWorldPoint(Input.mousePosition);
+                    _targetPos = new Vector3(mousePos.x - GrabAnchor.x, mousePos.y - GrabAnchor.y, transform.position.z);
+                }
+            }
         }
 
         protected void FixedUpdate()
         {
+            if (Started)
+            {
+                if (Picked)
+                {
+                    if (_rigidbody.useGravity)
+                    {
+                        _rigidbody.useGravity = false;
+                    }
+                    _rigidbody.MovePosition(_targetPos);
+                }
+                else if (!Picked && !_rigidbody.useGravity)
+                {
+                    _rigidbody.velocity = Vector3.zero;
+                    _rigidbody.useGravity = true;
+                }
+            }
 
         }
 
@@ -30,14 +61,16 @@ namespace NuclearCell
             var ray = GameManager.Singleton.WallCam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hit, 100, thisOnly))
             {
-
+                Picked = true;
+                Started = true;
+                GrabAnchor = hit.point - _rigidbody.position;
             }
             gameObject.layer = ogLayer;
         }
 
         protected void OnMouseUp()
         {
-            Debug.Log("Bye");
+            Picked = false;
         }
     }
 }
